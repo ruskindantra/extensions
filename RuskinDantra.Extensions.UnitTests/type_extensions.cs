88953bbb3c2bf6eec7f3c6af93ca632064647e8e
Ruskin Dantra <ruskin.dantra@promapp.com>
2016-01-03 19:00:43 +1300

    Added more unit tests

--- RuskinDantra.Extensions.UnitTests/type_extensions.cs
+++ RuskinDantra.Extensions.UnitTests/type_extensions.cs
@@ -19,6 +19,20 @@ namespace RuskinDantra.Extensions.UnitTests
 			var type = typeof(test_class_no_interface);
 			type.HasInterface<itest_interface>().Should().BeFalse();
 		}
+
+		[Test]
+		public void get_interface_should_not_return_null_if_interface_exists()
+		{
+			var type = typeof(test_class);
+			type.GetInterface<itest_interface>().Should().NotBeNull();
+		}
+
+		[Test]
+		public void get_interface_should_return_null_if_no_interface_exists()
+		{
+			var type = typeof(test_class_no_interface);
+			type.GetInterface<itest_interface>().Should().BeNull();
+		}
 	}
 
 	public interface itest_interface
--- RuskinDantra.Extensions/TypeExtensions.cs
+++ RuskinDantra.Extensions/TypeExtensions.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace RuskinDantra.Extensions
 {
     public static class TypeExtensions
     {
-		public static bool HasInterface<TInterface>(this Type type)
+		public static bool HasInterface<TInterface>([NotNull] this Type type)
 			where TInterface : class
 		{
 			return GetInterface<TInterface>(type) != null;
 		}
 
-		public static Type GetInterface<TInterface>(this Type type)
+		[CanBeNull]
+		public static Type GetInterface<TInterface>([NotNull] this Type type)
 			where TInterface : class
 		{
 			return type.GetInterface(typeof(TInterface).Name);
