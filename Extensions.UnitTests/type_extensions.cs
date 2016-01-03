31e81196152aee25e57945c622ea89eec377c082
Ruskin Dantra <ruskin.dantra@promapp.com>
2016-01-03 16:30:50 +1300

    Added type extensions

---
+++ Extensions.UnitTests/type_extensions.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Extensions.UnitTests
+{
+	[TestFixture]
+    public class type_extensions
+    {
+		[Test]
+	    public void has_interface_should_return_true_if_type_implements_interface()
+		{
+			var type = typeof (test_class);
+			type.HasInterface<itest_interface>().Should().BeTrue();
+		}
+
+		[Test]
+		public void has_interface_should_return_false_if_type_does_not_implement_interface()
+		{
+			var type = typeof(test_class_no_interface);
+			type.HasInterface<itest_interface>().Should().BeFalse();
+		}
+	}
+
+	public interface itest_interface
+	{
+		
+	}
+
+	public class test_class : itest_interface
+	{
+		
+	}
+
+	public class test_class_no_interface
+	{
+		
+	}
+}
---
+++ Extensions/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class TypeExtensions
+    {
+		public static bool HasInterface<TInterface>(this Type type)
+			where TInterface : class
+		{
+			return GetInterface<TInterface>(type) != null;
+		}
+
+		public static Type GetInterface<TInterface>(this Type type)
+			where TInterface : class
+		{
+			return type.GetInterface(typeof(TInterface).Name);
+		}
+	}
+}
